7ce24d181fb487ee7ab84e498879d9814b381b49
Ravi Ranjan <193577335+ravrranj@users.noreply.github.com>
2025-05-18 20:51:27 +0530

    Update StackUsingQueue.java
    Structure & Logic:
    
    You're using two queues: q1 (main), and q2 (helper).
    
    For pop() and peek(), you move all but one element to q2, then:
    
    pop() removes the last (top) element.
    
    peek() views it, and puts it back into the new q1.
    
    After each operation, you swap q1 and q2 to reset.

--- Documents/Java/array-practice-java/Stack_and_Queue/StackUsingQueue.java
+++ Documents/Java/array-practice-java/Stack_and_Queue/StackUsingQueue.java
@@ -7,9 +7,12 @@ public class StackUsingQueue {
     private Queue<Integer> q1 = new LinkedList<>();
     private Queue<Integer> q2 = new LinkedList<>();
 
+    // push is O(1)
     public void push(int x) {
         q1.add(x);
     }
+
+    // pop() and peek() are O(n) ❗(because of queue rotation)
     public int pop() {
         if (q1.isEmpty()) {
             System.out.println("Stack is empty.");
